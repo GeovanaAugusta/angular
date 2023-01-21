b82bc763806df98e3bf3af549bf3c14b515485c1
Geovana Augusta <geovanaag@hotmail.com>
2023-01-21 15:56:10 -0300

    feat: data binding - property binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -10,6 +10,9 @@ export class DataBindingComponent implements OnInit {
   public nome: string = "Geovana";
   public idade: number = 25;
   public valorUm: number = 1;
+  public checkedDisabled: boolean = true;
+  public imgSrc: string = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png"
+  public imgTitle: string = "Property Binding"
 
   ngOnInit(): void {
 
