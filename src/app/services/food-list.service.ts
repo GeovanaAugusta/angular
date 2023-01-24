58e1c3bb38f2f3a4e4419877fc6079310eeba94e
Geovana Augusta <geovanaag@hotmail.com>
2023-01-24 16:46:03 -0300

    feat: service - injeção de dependência

---
+++ src/app/services/food-list.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+// Toda aplicação passa a ter acesso ao mesmo
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodListService {
+
+  // Mais sentido limitar o acesso à somente esse serviço
+  private list: Array<string> = [
+    "Torta",
+    "Bolo",
+    "Sanduíche"
+  ];
+
+  constructor() { }
+
+  public foodList() {
+    return this.list;
+  }
+
+
+}
