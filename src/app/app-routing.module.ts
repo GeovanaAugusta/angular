c72741eb82a69cc0ddc5439d569ded98a70e8bb7
Geovana Augusta <geovanaag@hotmail.com>
2023-01-27 17:01:57 -0300

    feat: cria rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 // Componentes
-import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+// import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
-const routes: Routes = [{
-  path: '',
-  // Dessa maneira não há necessidade de referenciar a tag do componente em questão no app.component
-  component: ReactiveFormsComponent
-}];
+// const routes: Routes = [{
+//   path: '',
+//   // Dessa maneira não há necessidade de referenciar a tag do componente em questão no app.component
+//   component: ReactiveFormsComponent,
+//   // Quando a path inicia com valo vazio
+//   pathMatch: 'full'
+// },
+
+const routes: Routes = [
+{ path: '', component: HomeComponent, pathMatch: 'full' },
+{ path: 'sobre', component: SobreComponent, pathMatch: 'full' },
+{ path: '404', component: NotFoundComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+// Módulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
+
+// Importação do arquivo de rotas
 import { AppRoutingModule } from './app-routing.module';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DiretivasEstruturaisComponent } from './diretivas-estruturais/diretivas-estruturais.component';
 import { DiretivasAtributosComponent } from './diretivas-atributos/diretivas-atributos.component';
 import { PipesComponent } from './pipes/pipes.component';
-import { SharedModule } from './shared/shared.module';
+
 
 // Importação do reactive forms module
 import { FormsComponent } from './forms/forms.component';
 import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+
+// Pages
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +35,9 @@ import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.componen
     PipesComponent,
     FormsComponent,
     ReactiveFormsComponent,
+    HomeComponent,
+    SobreComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
