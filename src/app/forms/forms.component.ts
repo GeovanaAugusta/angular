c47723e6eff653a5d2eab805b37b133509b81ca7
Geovana Augusta <geovanaag@hotmail.com>
2023-01-27 14:56:06 -0300

    feat: select + ngForm

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { string } from 'joi';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss']
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
+
+  public listComidas: Array<{comida: string, preco: string}> = [
+    { comida: "Bolo", preco: "R$10,00" },
+    { comida: "Torta", preco: "R$15,00" },
+    { comida: "Bala", preco: "R$1,00" },
+  ]
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
 
 }
